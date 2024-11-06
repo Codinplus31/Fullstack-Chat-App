4b87371e391eb85df7176e810942959e7c233a47
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-06 07:55:08 +0100

    Update message.module.ts

--- server/src/message/message.module.ts
+++ server/src/message/message.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from '../user/user.module';
+import { UserModule } from '../../user/user.module';
 import { MessageController } from './message.controller';
 import { MessageProvider } from './message.provider';
 import { MessageService } from './message.service';
