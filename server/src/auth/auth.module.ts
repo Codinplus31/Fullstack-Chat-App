8f81ab8133f39633d0e43e3801f5d27b598f0d0b
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-06 06:56:13 +0100

    Update auth.module.ts

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserModule } from '../user/user.module';
+import { UserModule } from '../../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { JWT_SECRET } from '../utils/constants';
+import { JWT_SECRET } from '../../utils/constants';
 
 @Module({
   imports: [
