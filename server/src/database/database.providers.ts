f290005d15d6e703fc3c9cb7b0e37e97efbf2f43
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-06 08:10:54 +0100

    Update database.providers.ts

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -8,7 +8,7 @@ export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
+     /* const sequelize = new Sequelize({
         dialect: 'postgres',
        host: 'bocxnrhccosiswskpz82-postgresql.services.clever-cloud.com',
         port: 50013,
@@ -16,7 +16,16 @@ export const databaseProviders = [
         password: '4QwqdfuWPtew9xA1LvkvZADnKB5apP',
         database: 'bocxnrhccosiswskpz82'
         //connectionString: 'postgresql://uqzm1kkmllkly5xhheet:4QwqdfuWPtew9xA1LvkvZADnKB5apP@bocxnrhccosiswskpz82-postgresql.services.clever-cloud.com:50013/bocxnrhccosiswskpz82'
-              });
+              });*/
+
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: process.env.DB_PASSWORD,
+        database: 'link-chat-app'
+      });
       sequelize.addModels([User, Message, Channel]);
       await sequelize.sync();
       return sequelize;
