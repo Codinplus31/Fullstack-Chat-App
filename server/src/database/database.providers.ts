86c2f4dc59ab5bce5a79878518924f9dc925ec1f
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-07 09:06:46 +0100

    Update database.providers.ts

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { Channel } from '../channel/channel.entity';
 import { Message } from '../message/message.entity';
 import { User } from '../user/user.entity';
-
+import { pg } from "pg";
 
 export const databaseProviders = [
   {
@@ -14,7 +14,8 @@ export const databaseProviders = [
         port: 50013,
         username: 'uqzm1kkmllkly5xhheet',
         password: '4QwqdfuWPtew9xA1LvkvZADnKB5apP',
-        database: 'bocxnrhccosiswskpz82'
+        database: 'bocxnrhccosiswskpz82',
+           dialectModule: pg
         //connectionString: 'postgresql://uqzm1kkmllkly5xhheet:4QwqdfuWPtew9xA1LvkvZADnKB5apP@bocxnrhccosiswskpz82-postgresql.services.clever-cloud.com:50013/bocxnrhccosiswskpz82'
               });
 
